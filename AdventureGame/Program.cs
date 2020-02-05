6e17eafa005f24ba2790a706361a4ff3173da9c6
Melissa 't Hart <mhart@intersoftware.nl>
2020-02-05 16:21:38 +0100

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adventure Game!");
+            // Using Console.ReadKey() to keep app window from closing immediately 
+            Console.ReadKey();
+        }
+    }
+}
